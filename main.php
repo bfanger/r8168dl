40db6a80d057727b7c02b955cee2ce9fe7135279
Bob Fanger <bfanger@gmail.com>
2016-04-08 11:50:05 +0200

    Added 8.041.001

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Realtek r8168 linux driver</title>
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"></link>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -19,25 +19,29 @@
 
 		<h1>Realtek r8168 linux driver</h1>
 		<strong>Works with:</strong><br />
-		RTL8111B / RTL8168B / RTL8111/RTL8168<br />
+		RTL8111B / RTL8168B / RTL8111 / RTL8168<br />
 		RTL8111C / RTL8111CP / RTL8111D(L)<br />
 		RTL8168C / RTL8111DP / RTL8111E<br />
 		RTL8168E / RTL8111F / RTL8411<br />
 		RTL8111G / RTL8111GUS / RTL8411B(N)<br />
+		RTL8118AS<br />
+
 		<br />
 
 
 		These files are downloaded from the <a href="http://www.realtek.com.tw/downloads/downloadsView.aspx?Langid=1&amp;PNid=13&amp;PFid=5&amp;Level=5&amp;Conn=4&amp;DownTypeID=3&amp;GetDown=false">Realtek download page</a>
 		<h2>Downloads</h2>
 		<ul class="list-unstyled">
-			<li><a href="files/r8168-8.039.00.tar.bz2" onclick="ga('send', 'event', this.href, 'click');">r8168-8.039.00.tar.bz2 <span class="label label-primary">Latest</span></a></li>
-			<li><a href="files/r8168-8.038.00.tar.bz2" onclick="ga('send', 'event', this.href, 'click');">r8168-8.038.00.tar.bz2</a></li>
+			<li><a href="files/r8168-8.041.01.tar.bz2" onclick="ga('send', 'event', this.href, 'click');">r8168-8.041.01.tar.bz2 <span class="label label-primary">Latest</span></a></li>
+			<li><a href="files/r8168-8.040.00.tar.bz2" onclick="ga('send', 'event', this.href, 'click');">r8168-8.040.00.tar.bz2</a></li>
 		</ul>
 		<p>
 			These download links can be used from within a build script.<br />
-			It is used in the <a href="http://www.archlinux.org/packages/community/i686/r8168/" target="_blank">PKGBUILD</a> for <a href="http://archlinux.org" target="_target">ArchLinux</a>.
+			It was used in the <a href="http://www.archlinux.org/packages/community/i686/r8168/" target="_blank">PKGBUILD</a> for <a href="http://archlinux.org" target="_target">ArchLinux</a>.
 		</p>
-		Visit <a href="http://www.realtek.com.tw/" target="_blank">http://www.realtek.com.tw/</a> for support.
+		<p>
+			Visit <a href="http://www.realtek.com.tw/" target="_blank">http://www.realtek.com.tw/</a> for support. Missing the latest version? <a href="https://github.com/bfanger/r8168dl">open a github issue</a>
+		</p>
 	</div>
 
 </body>
